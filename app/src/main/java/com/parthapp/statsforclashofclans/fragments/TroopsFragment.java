c3ccd10bfe17c3586c1d1f27721fd7d2b62c1cc6
Mann Patel <you@example.com>
2021-03-29 21:21:06 -0400

    Fragments redone

--- app/src/main/java/com/parthapp/statsforclashofclans/fragments/TroopsFragment.java
+++ app/src/main/java/com/parthapp/statsforclashofclans/fragments/TroopsFragment.java
@@ -1,32 +1,32 @@
-package com.parthapp.statsforclashofclans;
+package com.parthapp.statsforclashofclans.fragments;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.parthapp.statsforclashofclans.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SecondFragment#newInstance} factory method to
+ * Use the {@link TroopsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SecondFragment extends Fragment {
+public class TroopsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    public static final String TAG = "SECOND FRAG";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public SecondFragment() {
+    public TroopsFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class SecondFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SecondFragment.
+     * @return A new instance of fragment TroopsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SecondFragment newInstance(String param1, String param2) {
-        SecondFragment fragment = new SecondFragment();
+    public static TroopsFragment newInstance(String param1, String param2) {
+        TroopsFragment fragment = new TroopsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -51,7 +51,6 @@ public class SecondFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "SECOND FRAG");
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -61,8 +60,7 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_troops, container, false);
     }
 }
